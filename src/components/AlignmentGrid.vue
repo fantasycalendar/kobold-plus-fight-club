ffa72184b99ea17c8909d7a5f6cf5567a03e47cb
V13Axel <alex@stechstudio.com>
2022-05-30 14:28:40 -0400

    Working alignment grid

---
+++ src/components/AlignmentGrid.vue
@@ -0,0 +1,231 @@
+<template>
+  <div>
+    <div class="block font-medium text-gray-700 dark:text-gray-300 mb-1">
+      Alignment
+    </div>
+
+    <div
+      class="grid grid-cols-[12px_12px_repeat(12,_minmax(0,_1fr))] divide divide-gray-200 dark:divide-gray-600 gap-2 rounded-md bg-white dark:bg-gray-700 border border-gray-200 dark:border-gray-600 overflow-hidden p-2"
+    >
+      <div
+        @click="toggle(lawful_good | neutral | chaotic_evil)"
+        :class="classes(lawful_good | neutral | chaotic_evil)"
+        class="col-span-2 rounded-tl-md dark:border-gray-600"
+      ></div>
+      <div
+        @click="toggle(lawful_good | lawful_neutral | lawful_evil)"
+        :class="classes(lawful_good | lawful_neutral | lawful_evil)"
+        class="col-span-4 h-8 rounded-t-md"
+      >
+        <i class="fa fa-gavel"></i>
+      </div>
+      <div
+        @click="toggle(neutral_good | neutral | neutral_evil)"
+        :class="classes(neutral_good | neutral | neutral_evil)"
+        class="col-span-4 h-8 rounded-t-md"
+      >
+        <i class="fa fa-scale-balanced"></i>
+      </div>
+      <div
+        @click="toggle(chaotic_good | chaotic_neutral | chaotic_evil)"
+        :class="classes(chaotic_good | chaotic_neutral | chaotic_evil)"
+        class="col-span-4 h-8 rounded-t-md"
+      >
+        <i :class="{[randomIcon]: true}"></i>
+      </div>
+
+      <div
+        @click="toggle(lawful_good | neutral_good | chaotic_good)"
+        :class="classes(lawful_good | neutral_good | chaotic_good)"
+        class="col-span-2 w-8 rounded-l-md"
+      >
+        <i class="fa fa-dove"></i>
+      </div>
+      <div
+        @click="toggle(lawful_good)"
+        :class="classes(lawful_good)"
+        class="col-span-4 h-12"
+        title="Lawful Good"
+      >
+        LG
+      </div>
+      <div
+        @click="toggle(neutral_good)"
+        :class="classes(neutral_good)"
+        class="col-span-4 h-12"
+        title="Neutral Good"
+      >
+        NG
+      </div>
+      <div
+        @click="toggle(chaotic_good)"
+        :class="classes(chaotic_good)"
+        class="col-span-4 h-12"
+        title="Chaotic Good"
+      >
+        CG
+      </div>
+
+      <div
+        @click="toggle(lawful_neutral | neutral | chaotic_neutral)"
+        :class="classes(lawful_neutral | neutral | chaotic_neutral)"
+        class="col-span-2 w-8 rounded-l-md"
+      >
+        <i class="fa fa-face-meh"></i>
+      </div>
+      <div
+        @click="toggle(lawful_neutral)"
+        :class="classes(lawful_neutral)"
+        class="col-span-4 h-12"
+        title="Lawful Neutral"
+      >
+        LN
+      </div>
+      <div
+        @click="toggle(neutral)"
+        :class="classes(neutral)"
+        class="col-span-4 h-12"
+        title="Neutral"
+      >
+        N
+      </div>
+      <div
+        @click="toggle(chaotic_neutral)"
+        :class="classes(chaotic_neutral)"
+        class="col-span-4 h-12"
+        title="Chaotic Neutral"
+      >
+        CN
+      </div>
+
+      <div
+        @click="toggle(lawful_evil | neutral_evil | chaotic_evil)"
+        :class="classes(lawful_evil | neutral_evil | chaotic_evil)"
+        class="col-span-2 w-8 rounded-l-md"
+      >
+        <i class="fa fa-skull"></i>
+      </div>
+      <div
+        @click="toggle(lawful_evil)"
+        :class="classes(lawful_evil)"
+        class="col-span-4 h-12"
+        title="Lawful Evil"
+      >
+        LE
+      </div>
+      <div
+        @click="toggle(neutral_evil)"
+        :class="classes(neutral_evil)"
+        class="col-span-4 h-12"
+        title="Neutral Evil"
+      >
+        NE
+      </div>
+      <div
+        @click="toggle(chaotic_evil)"
+        :class="classes(chaotic_evil)"
+        class="col-span-4 h-12"
+        title="Chaotic Evil"
+      >
+        CE
+      </div>
+
+      <div
+        @click="toggle(lawful_evil | neutral | chaotic_good)"
+        :class="classes(lawful_evil | neutral | chaotic_good)"
+        class="col-span-2 rounded-bl-md"
+      ></div>
+      <div
+        @click="total = total === 1023 ? 0 : 1023"
+        :class="{
+          'bg-emerald-700 text-white hover:bg-emerald-600 dark:bg-emerald-800 dark:hover:bg-emerald-900':
+            (total & 1023) === 1023,
+          'text-gray-900 bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 border':
+            (total & 1023) !== 1023,
+        }"
+        class="select-none text-gray-900 col-span-6 cursor-pointer grid place-items-center h-8 bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 border dark:text-white"
+      >
+        Any
+      </div>
+      <div
+        @click="total = total ^ 512"
+        :class="{
+          'bg-emerald-700 text-white hover:bg-emerald-600 dark:bg-emerald-800 dark:hover:bg-emerald-900':
+            (total & 512) === 512,
+          'text-gray-900 bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 border':
+            (total & 512) !== 512,
+        }"
+        class="select-none text-gray-900 rounded-br-md col-span-6 cursor-pointer grid place-items-center h-8 bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 border dark:text-white"
+      >
+        Unaligned
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch, computed } from "vue";
+
+const lawful_good = ref(1);
+const neutral_good = ref(2);
+const chaotic_good = ref(4);
+const lawful_neutral = ref(8);
+const neutral = ref(16);
+const chaotic_neutral = ref(32);
+const lawful_evil = ref(64);
+const neutral_evil = ref(128);
+const chaotic_evil = ref(256);
+const total = ref(1023);
+const randomIconIndex = ref(0);
+const icons = ref([
+  "fa-solid fa-dumpster-fire",
+  "fa-solid fa-poo-storm",
+  "fa-solid fa-meteor",
+  "fa-solid fa-arrows-split-up-and-left",
+  "fa-solid fa-arrows-turn-to-dots",
+  "fa-solid fa-candy-cane",
+  "fa-solid fa-stroopwafel",
+  "fa-solid fa-cake-candles",
+  "fa-solid fa-bowling-ball",
+  "fa-solid fa-bath",
+  "fa-solid fa-bomb",
+  "fa-solid fa-business-time",
+  "fa-solid fa-dragon",
+  "fa-solid fa-arrow-up-right-dots",
+  "fa-solid fa-fire",
+  "fa-solid fa-arrows-to-circle",
+  "fa-solid fa-cloud-meatball",
+  "fa-solid fa-arrows-down-to-people",
+  "fa-solid fa-person-walking-arrow-loop-left",
+  "fa-solid fa-person-walking-dashed-line-arrow-right",
+]);
+const randomIcon = computed(() => icons.value[randomIconIndex.value]);
+
+watch(total, (total) => {
+  let newIndex = Math.floor(Math.random() * icons.value.length);
+  randomIconIndex.value =
+    newIndex === randomIconIndex.value
+      ? (newIndex + 1) % icons.value.length
+      : newIndex;
+});
+
+function toggle(value) {
+  this.total =
+    (this.total & value) > 0
+      ? (this.total | value) ^ value
+      : this.total | value;
+}
+
+function classes(value) {
+  return {
+    "bg-emerald-700 hover:bg-emerald-600 dark:bg-emerald-800 dark:hover:bg-emerald-900 text-white":
+      this.total & value,
+    "bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 border text-gray-700 dark:text-white":
+      !(this.total & value),
+    "cursor-pointer grid place-items-center text-center select-none text-white": true,
+  };
+}
+function reset() {
+  this.total = 1023;
+}
+</script>
