38b5dd5233220609d980330f171f9b4a8ecd8595
Alex Dills <alex@stechstudio.com>
2023-08-08 09:50:32 -0400

    Dependency Updates (#16)
    * Initializing Dependency_Updates for empty Pull Request
    
    * update deps
    
    * Add LocalValetDriver
    
    * Silly little build needed page

---
+++ LocalValetDriver.php
@@ -0,0 +1,53 @@
+<?php
+
+class LocalValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return true;
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (file_exists($staticFilePath = $sitePath.'/dist/'.$uri)) {
+            return $staticFilePath;
+        }
+
+        if (file_exists($staticFilePath = $sitePath.'/public/'.$uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath.'/dist/index.html')
+            ? $sitePath.'/dist/index.html'
+            : $sitePath.'/build_needed.html';
+    }
+}
